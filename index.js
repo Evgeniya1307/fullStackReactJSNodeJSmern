56e4560f9c87af8ddb7cbe48a8654e6b77b97f6d
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-06 14:41:15 +0400

    сгенерировать токен  const token

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import express from "express"; //подключила express
+import jwt from "jsonwebtoken"
 
+// express будет хранится в app
 const app = express(); //соз-ла экспресс приложение
+
+// позволит читать JSON в запросах
+app.use(express.json());
 
 app.get("/", (req, res) => {
   //(req-что прислал клиент,res-что передаю обратно)
   res.send("Hello World");
 });
 
-app.post('/autch/login')//отлавливаю запрос
+// авторизация
+app.post('/auth/login', (req, res) => {
+  
+  //сгенерировать токен
+ const token= jwt.sign({
+    email: req.body.email,
+    fullName: 'Вася Пупкин'
+  },
+  'secret123',
+  );//(в скобках ук-ю что шифрую) 'с помощью ключа'
+  
+  res.json({
+    success: true,
+    token,
+  });
+}); //отлавливаю запрос
 
 app.listen(4444, (err) => {
   if (err) {
