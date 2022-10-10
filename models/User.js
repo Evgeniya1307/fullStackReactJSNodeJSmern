1b0f237a395948d4cf63b9a1fc33408c99e84089
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-10 14:00:09 +0400

    timestamps:true //обязательно должны быть создание и обновление даты

--- models/User.js
+++ models/User.js
@@ -3,13 +3,21 @@ import mongoose from "mongoose";
 
 //схема моей табл
 const UserSchema = new mongoose.Schema({
-fuulName:{ //св-во
+  fuulName: {
+    //св-во
     type: String, //яв-ся строчкой
-    required: true, //яв-ся обязательной
-},
-email:{
-    type: String, //яв-ся строчкой
-    required: true, //яв-ся обязательной
+    required: true, //яв-ся обязательной для заполнения
+  },
+  email: {
+    type: String,
+    required: true,
     unique: true, //почта уникальная(нельзя несколько одинаковых почт в базе данных)
-}
-})
+  },
+  passwordHash: {
+    type: String, //инфа может быть о пароле но зашифрованна
+    required: true,
+  },
+  avatarUrl:String //у пользователя есть аватар
+},{
+   timestamps:true //обязательно должны быть создание и обновление даты
+});
