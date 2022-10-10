4476ec6600ccda16e568633b3f3bafefc882c6eb
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-10 13:54:36 +0400

    unique: true, //почта уникальная(нельзя несколько одинаковых почт в базе данных)

--- models/User.js
+++ models/User.js
@@ -1,1 +1,15 @@
 //делаю структуру списка пользователей
+import mongoose from "mongoose";
+
+//схема моей табл
+const UserSchema = new mongoose.Schema({
+fuulName:{ //св-во
+    type: String, //яв-ся строчкой
+    required: true, //яв-ся обязательной
+},
+email:{
+    type: String, //яв-ся строчкой
+    required: true, //яв-ся обязательной
+    unique: true, //почта уникальная(нельзя несколько одинаковых почт в базе данных)
+}
+})
