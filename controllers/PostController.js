631a187d2a35a9c7deafd34f5cd4fb2c5be762d2
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-12 11:21:39 +0400

    //возвращаю ответ

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,14 +1,20 @@
 import PostModel from "../models/Post";
 
 // создание статей
-export const create =(req, res, ) =>{
+export const create = async(req, res, ) =>{
     try{
 const doc = new PostModel({ //создала документ,ук-ла что есть постах (body-это то что пользователь передаёт)
 title: req.body.title,
 text: req.body.text,
 imageUrl: req.body.imageUrl,
 tags: req.body.tags,
+user: req.userId,//то что доверяю бэку вытаскиваю
 })
+
+//когда док подготовлен его нужно создать
+const post = await document.save()
+//возвращаю ответ
+
     } catch(err){
 
     }
